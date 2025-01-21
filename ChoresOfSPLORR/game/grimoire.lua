a134db380b488e74d402679ad4db5df682a11c44
TheGrumpyGameDev <ernestpazera@gmail.com>
2025-01-21 04:24:53 -0600

    restart the game and mute the sfx

--- ChoresOfSPLORR/game/grimoire.lua
+++ ChoresOfSPLORR/game/grimoire.lua
@@ -13,16 +13,13 @@ M.MSG_MOVE_UP = "move up"
 M.MSG_MOVE_DOWN = "move down"
 M.MSG_MOVE_LEFT = "move left"
 M.MSG_MOVE_RIGHT = "move right"
+M.MSG_RESTART = "restart"
 M.URL_GUI = "/go#gui"
 M.URL_SCENE = "/scene"
 M.URL_MOVES_LABEL = "/scene#moves_label"
 M.URL_SCORE_LABEL = "/scene#score_label"
 M.GUI_MESSAGE_BOX = "message_box/box"
 M.GUI_MESSAGE_BOX_TEXT = "message_box/text"
-M.GUI_UP_ARROW = "up_arrow"
-M.GUI_DOWN_ARROW = "down_arrow"
-M.GUI_LEFT_ARROW = "left_arrow"
-M.GUI_RIGHT_ARROW = "right_arrow"
 M.INTENSITY_TILE_START = 245
 
 return M
